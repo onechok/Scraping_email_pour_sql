f3ab25373b14a58d7b209eabb682e895cf6cd1c0
onechok <contact@onechok.ch>
2023-04-16 01:42:18 +0200

    bs4 et lxml

--- database.py
+++ database.py
@@ -7,20 +7,31 @@ def connect_to_db():
         connection = pymysql.connect(**db_config)
         print("Connexion à la base de données MariaDB réussie.")
     except Exception as e:
-        print("Erreur de connexion à la base de données MariaDB :", e)
+        print(f"Erreur de connexion à la base de données MariaDB : {e}")
         connection = None
     return connection
 
 # Création de la table dans la base de données
 def create_table(connection):
-    if connection:
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute("CREATE TABLE IF NOT EXISTS emails (id INT NOT NULL AUTO_INCREMENT, nom VARCHAR(255) DEFAULT '', mail VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
-                connection.commit()
-                print("Table 'emails' créée avec succès dans la base de données 'scrape'.")
-        except Exception as e:
-            print("Erreur de création de la table 'emails' :", e)
+    if not connection:
+        print("Aucune connexion à la base de données pour créer la table.")
+        return
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS emails (
+                id INT NOT NULL AUTO_INCREMENT,
+                nom VARCHAR(255) DEFAULT '',
+                mail VARCHAR(255) NOT NULL,
+                url VARCHAR(255) NOT NULL,
+                PRIMARY KEY (id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+            """)
+            connection.commit()
+            print("Table 'emails' créée avec succès dans la base de données 'scrape'.")
+    except Exception as e:
+        print(f"Erreur de création de la table 'emails' : {e}")
 
 # Stockage des e-mails dans la base de données
 def store_emails(connection, titre, mails, url):
@@ -32,25 +43,16 @@ def store_emails(connection, titre, mails, url):
         print("Aucune information à stocker dans la base de données.")
         return
 
-    if mails:
-        for mail in mails:
-            try:
-                with connection.cursor() as cursor:
-                    sql = "INSERT INTO emails (nom, mail, url) VALUES (%s, %s, %s)"
-                    cursor.execute(sql, (titre, mail, url))
-                    connection.commit()
-                print("Adresse e-mail enregistrée :", mail)
-            except Exception as e:
-                print("Erreur d'enregistrement de l'adresse e-mail :", e)
-    else:
+    mails_to_store = mails if mails else [""]
+    for mail in mails_to_store:
         try:
             with connection.cursor() as cursor:
                 sql = "INSERT INTO emails (nom, mail, url) VALUES (%s, %s, %s)"
-                cursor.execute(sql, (titre, "", url))
+                cursor.execute(sql, (titre, mail, url))
                 connection.commit()
-            print("Aucune adresse e-mail trouvée. Enregistrement du titre et de l'URL.")
+            print(f"Adresse e-mail enregistrée : {mail}")
         except Exception as e:
-            print("Erreur d'enregistrement du titre et de l'URL :", e)
+            print(f"Erreur d'enregistrement de l'adresse e-mail : {e}")
 
 # Affichage des données stockées dans la base de données
 def display_data(connection, url):
@@ -72,9 +74,9 @@ def display_data(connection, url):
                 for result in results:
                     print(result['nom'], result['mail'], result['url'])
             else:
-                print("Aucune information trouvée dans la base de données pour l'URL :", url)
+                print(f"Aucune information trouvée dans la base de données pour l'URL : {url}")
     except Exception as e:
-        print("Erreur de récupération des informations depuis la base de données :", e)
+        print(f"Erreur de récupération des informations depuis la base de données : {e}")
 
 # Fermeture de la connexion à la base de données
 def close_db_connection(connection):
@@ -83,4 +85,4 @@ def close_db_connection(connection):
             connection.close()
             print("Fermeture de la connexion à la base de données MariaDB réussie.")
         except Exception as e:
-            print("Erreur de fermeture de la connexion à la base de données MariaDB :", e)
+            print(f"Erreur de fermeture de la connexion à la base de données MariaDB : {e}")
--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import lxml.html
 
 # Récupération du contenu HTML
 def fetch_html(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'}
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         html_content = response.content
         print("Récupération du contenu HTML réussie.")
@@ -15,18 +17,39 @@ def fetch_html(url):
     return html_content
 
 # Extraction des e-mails du contenu HTML
-def extract_emails(html_content):
+def extract_emails(html_content, ignore_case=True):
     if not html_content:
         print("Aucun contenu HTML fourni pour extraire les e-mails.")
         return None, set()
 
     soup = BeautifulSoup(html_content, 'html.parser')
-    titre = soup.title.string
+    titre = soup.title.string.strip() if soup.title else None
     if not titre:
         print("Impossible d'extraire le titre du site.")
-    mails = set(re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', soup.get_text()))
+
+    regex_flags = re.IGNORECASE if ignore_case else 0
+    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    mails = set(re.findall(email_pattern, soup.get_text(), regex_flags))
+
+    for tag in soup.find_all(True):
+        for attr in ['href', 'src', 'data']:
+            if tag.has_attr(attr):
+                attribute_value = tag[attr]
+                if isinstance(attribute_value, str):
+                    mails.update(re.findall(email_pattern, attribute_value, regex_flags))
 
     if not mails:
-        print("Aucune adresse e-mail trouvée dans le contenu HTML.")
+        print("Aucune adresse e-mail trouvée dans le contenu HTML avec BeautifulSoup. Tentative avec lxml.")
+        root = lxml.html.fromstring(html_content)
+        mails = set(re.findall(email_pattern, root.text_content(), regex_flags))
+
+        for element in root.iter():
+            for attr in ['href', 'src', 'data']:
+                attribute_value = element.get(attr)
+                if attribute_value:
+                    mails.update(re.findall(email_pattern, attribute_value, regex_flags))
+
+        if not mails:
+            print("Aucune adresse e-mail trouvée dans le contenu HTML avec lxml.")
 
     return titre, mails
